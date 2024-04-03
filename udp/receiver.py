e67e960614946198848f60c5b7414534f27c501a
alexiadorneles <alexiadorneles02@gmail.com>
2024-04-02 23:26:58 -0300

    wip

---
+++ udp/receiver.py
@@ -0,0 +1,26 @@
+import socket
+
+# Server configuration
+HOST = '127.0.0.1'  # Loopback address
+PORT = 12345        # Port to listen on
+BUFFER_SIZE = 1024  # Size of the buffer for receiving data
+END_MESSAGE = b"END_TRANSFER"  # Message to indicate end of file transfer
+
+def receive_file(filename):
+    # Create a UDP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        # Bind the socket to the address and port
+        server_socket.bind((HOST, PORT))
+        print(f"Server listening on {HOST}:{PORT}")
+        
+        # Receive data and write to the file
+        with open(filename, 'wb') as f:
+            while True:
+                data, addr = server_socket.recvfrom(BUFFER_SIZE)
+                if data == END_MESSAGE:
+                    break
+                f.write(data)
+            print("File received successfully")
+
+if __name__ == "__main__":
+    receive_file("received_file.txt")
---
+++ udp/sender.py
@@ -0,0 +1,25 @@
+import socket
+
+# Server configuration
+SERVER_HOST = '127.0.0.1'  # Server's IP address
+SERVER_PORT = 12345        # Port to send to
+BUFFER_SIZE = 1024         # Size of the buffer for sending data
+END_MESSAGE = b"END_TRANSFER"  # Message to indicate end of file transfer
+
+def send_file(filename):
+    # Create a UDP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
+        # Send the file
+        with open(filename, 'rb') as f:
+            print(f"Sending file '{filename}'")
+            while True:
+                data = f.read(BUFFER_SIZE)
+                if not data:
+                    break
+                client_socket.sendto(data, (SERVER_HOST, SERVER_PORT))
+            # Send end of file signal
+            client_socket.sendto(END_MESSAGE, (SERVER_HOST, SERVER_PORT))
+        print("File sent successfully")
+
+if __name__ == "__main__":
+    send_file("../large_file.txt")
